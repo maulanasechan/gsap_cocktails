18629219a90fd134d43007c7cf9b8dedd3a23183
maulana sechan <maulana.sechan@ecomindo.com>
2026-02-11 15:57:44 +0700

    Last Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,31 @@
+import { useGSAP } from "@gsap/react";
 import { openingHours, socials } from "../../constants";
 import { asset } from "../utils/assets";
+import { SplitText } from "gsap/all";
+import gsap from "gsap";
 
 const Contact = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", { type: "words" });
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+
+    timeline
+      .from(titleSplit.words, { opacity: 0, yPercent: 100, stagger: 0.02 })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-right-leaf", { y: "-50", duration: 1 }, "<")
+      .to("#f-left-leaf", { y: "50", duration: 1 }, "<");
+  }, []);
+
   return (
     <footer id="contact" className="contact-radial-gradient">
       <img
